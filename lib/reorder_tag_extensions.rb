fa0466f9d623924774e01cf6b607946ebdf342ee
Benny Degezelle <benny@gorilla-webdesign.be>
2011-06-07 16:30:46 +0200

    simplifying r:next and r:previous tags

--- lib/reorder_tag_extensions.rb
+++ lib/reorder_tag_extensions.rb
@@ -2,50 +2,36 @@ module ReorderTagExtensions
   include Radiant::Taggable
   
   desc %{
-    Page attribute tags inside this tag refer to the previous child page of the
-    contextual parent page. The by and status attributes work the same way they do in the
-    children:each tag. This tag only expands if the page exists.
+    Sets page context to next page sibling. For example:
+
+    <pre><code><r:next by="title"><r:link/></r:next></code></pre>
 
     *Usage:*
-    <pre><code><r:next [by="attribute"] [status="draft|reviewed|published|hidden|all"]>...</r:next></code></pre>
+    <pre><code><r:next [by="sort_order"]>...</r:next></code></pre>
   }
-  tag 'next' do |tag|
-    expand_tag_with_page_having_status(tag, tag.attr['status']) do
-      tag.locals.page.next(tag.attr['by'] || :position)
+  tag "next" do |tag|
+    by = tag.attr['by'] || "position"
+    next_page = Page.find_by_parent_id(tag.locals.page.parent_id, :conditions => ["virtual = false and status_id = 100 and ? > ?", by, tag.locals.page.send(by)], :order => "#{by} ASC", :limit => 1)
+    if next_page
+      tag.locals.page = next_page
+      tag.expand
     end
   end
-  
-  desc %{   
-    Page attribute tags inside this tag refer to the previous child page of the
-    contextual parent page. The by and status attributes work the same way they do in the 
-    children:each tag. This tag only expands if the page exists.
+
+  desc %{
+    Sets page context to previous page sibling. For example:
+    <pre><code><r:previous by="title"><r:link/></r:previous></code></pre>
 
     *Usage:*
-    <pre><code><r:previous [by="attribute"] [status="draft|reviewed|published|hidden|all"]>...</r:previous></code></pre>
+    <pre><code><r:previous [by="sort_order"]>...</r:previous></code></pre>
   }
-  tag 'previous' do |tag|
-    expand_tag_with_page_having_status(tag, tag.attr['status']) do
-      tag.locals.page.previous(tag.attr['by'] || :position)
+  tag "previous" do |tag|
+    by = tag.attr['by'] || "position"
+    previous_page = Page.find_by_parent_id(tag.locals.page.parent_id, :conditions => ["virtual = false and status_id = 100 and ? < ?", by, tag.locals.page.send(by)], :order => "#{by} DESC", :limit => 1)
+    if previous_page
+      tag.locals.page = previous_page
+      tag.expand
     end
   end
-  
-  private
-  
-    def expand_tag_with_page_having_status(tag, status)
-      if status
-        status_id = Status[status].id
-        equality = '='
-      else
-        status_id = Status[:draft].id
-        equality = '!='
-      end
-      target_page = Page.with_scope(:find => { :conditions => ["status_id #{equality} ?", status_id] }) do
-        yield
-      end
-      if target_page
-        tag.locals.page = target_page
-        tag.expand
-      end
-    end
   
 end
