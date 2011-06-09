d4e0656c659194a92dd98082e8490abd92abca95
Benny Degezelle <benny@gorilla-webdesign.be>
2011-06-09 02:41:54 +0200

    fixing order..

--- lib/reorder_tag_extensions.rb
+++ lib/reorder_tag_extensions.rb
@@ -11,7 +11,7 @@ module ReorderTagExtensions
   }
   tag "next" do |tag|
     by = tag.attr['by'] || "position"
-    next_page = Page.find_by_parent_id(tag.locals.page.parent_id, :conditions => ["virtual = false and status_id = 100 and ? > ?", by, tag.locals.page.send(by)], :order => "#{by} ASC", :limit => 1)
+    next_page = Page.find_by_parent_id(tag.locals.page.parent_id, :conditions => ["virtual = false and status_id = 100 and #{by} > ?", tag.locals.page.send(by)], :order => "#{by} ASC")
     if next_page
       tag.locals.page = next_page
       tag.expand
@@ -27,7 +27,7 @@ module ReorderTagExtensions
   }
   tag "previous" do |tag|
     by = tag.attr['by'] || "position"
-    previous_page = Page.find_by_parent_id(tag.locals.page.parent_id, :conditions => ["virtual = false and status_id = 100 and ? < ?", by, tag.locals.page.send(by)], :order => "#{by} DESC", :limit => 1)
+    previous_page = Page.find_by_parent_id(tag.locals.page.parent_id, :conditions => ["virtual = false and status_id = 100 and #{by} < ?", tag.locals.page.send(by)], :order => "#{by} DESC")
     if previous_page
       tag.locals.page = previous_page
       tag.expand
